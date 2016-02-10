da228d995c1d4fe3456caa8d4e50756c5be9331a
Nerissa Lin <lin.nerissa@berkeley.edu>
2016-02-09 17:02:17 -0800

    finished hw1

--- foobar.rb
+++ foobar.rb
@@ -1,3 +1,18 @@
 class Foobar
   # Q4 CODE HERE
+
+  
+
+  def self.baz(a)
+  	a = a.map{|x| x.to_i}
+  	a = a.map{|x| x + 2}
+  	a = a.keep_if{|x| x%2 == 0}
+  	a = a.uniq
+  	a = a.keep_if{|x| x<10}
+  	total = 0
+  	a.each{|x| total += x}
+  	return total
+  	
+  end
+
 end
--- hw1.rb
+++ hw1.rb
@@ -1,13 +1,21 @@
 def squared_sum(a, b)
-  # Q1 CODE HERE
+  return (a + b) * (a + b)
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
+  a = a.sort
+  x = 0
+  while x < a.length
+  	a[x] = a[x] + 1
+  	x = x + 1
+  end
+  return a
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
+  return first_name + " " + last_name
 end
 
 def blockin_time(a)
